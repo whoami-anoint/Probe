f2029d513790cbda8dd96cc8c64b9b87ea1c4a37
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-06-05 14:35:46 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-#Root check 
-if [ "$EUID" -ne 0 ]
-then
-    echo "*********************************************"
-    echo "* * * Please run the command as root  * * * *"
-    echo "* * * * * * \$sudo ./install.sh * * * * * * *"
-    echo "*********************************************"
-    exit
-fi
 # OS Check
 os_type=`uname -r`
 if [[ $os_type == *"arch1"* ]];
 then
-    pkg_manager="yay -S "
+    pkg_manager="yay -S ";
+    # go_lang = "go"
 else 
-    pkg_manager="apt-get install"
+    pkg_manager="sudo apt-get install";
+    go_lang = "golang"
 fi
-pv -qL 70 .banner.txt
-echo -e "\n[*]Installing Probe....." | pv -qL 8
+#Before running banner or other text pv is required
+
+$pkg_manager pv
+clear
+pv -qL 100 .banner.txt
+echo -e "\n\n[*]Installing Probe....." | pv -qL 8
+
+#Installing basic package
 $pkg_manager dirsearch 
-$pkg_manager nmap 
-$pkg_manager go 
+$pkg_manager nmap
+$pkg_manager $go_lang
+
 
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/OWASP/Amass/v3/...@master
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/tomnomnom/waybackurls@latest
-mv ~/go/bin/* /bin/
+go get -u github.com/tomnomnom/gf >/dev/null
+clear
+
+echo -e "[*]GF with good nature make life easy..." | pv -qL 8
+echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+echo -e "[*]Proposing gf...."|pv -qL 8
+echo 'source /root/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+echo -e "[*]Building Relation......"|pv -qL 8
+sudo mv ~/go/bin/* /bin/
+echo -e "\n[*]Probe is ready to use...."|pv -qL 8
+cat .thanks.txt
