377be613e87c2a4d19dca9dc9eef16020c86f230
Selenophile (M7) <63031654+m7-Selenophile@users.noreply.github.com>
2022-06-19 21:08:45 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,10 @@ go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/OWASP/Amass/v3/...@master
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/tomnomnom/waybackurls@latest
-go get -u github.com/tomnomnom/gf >/dev/null
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/tomnomnom/gf@latest >/dev/null
+git clone https://github.com/tomnomnom/gf 
+git clone https://github.com/1ndianl33t/Gf-Patterns
 clear
 
 echo -e "[*]GF with good nature make life easy..." | pv -qL 8
@@ -36,5 +39,9 @@ echo -e "[*]Proposing gf...."|pv -qL 8
 echo 'source /root/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
 echo -e "[*]Building Relation......"|pv -qL 8
 sudo mv ~/go/bin/* /bin/
+mkdir ~/.gf
+
+cp /gf/examples/* ~/.gf
+cp Gf-Patterns/* ~/.gf
 echo -e "\n[*]Probe is ready to use...."|pv -qL 8
 cat .thanks.txt
