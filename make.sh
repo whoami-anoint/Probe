36bbd2bbf38e7a5208b7ccae64aada7b39e536f1
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-06-20 01:21:10 +0545

    Update make.sh
    Signed-off-by: Anoint <72187543+whoami-anoint@users.noreply.github.com>

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ os_type=`uname -r`
 if [[ $os_type == *"arch1"* ]];
 then
     pkg_manager="yay -S ";
-    go_lang="go"
+    # go_lang = "go"
 else 
     pkg_manager="sudo apt-get install";
     go_lang="golang"
@@ -14,15 +14,16 @@ fi
 
 $pkg_manager pv
 clear
-pv -qL 100 .banner.txt
+bash script/art.sh
 echo -e "\n\n[*]Installing Probe....." | pv -qL 8
 
 #Installing basic package
+
 $pkg_manager dirsearch 
 $pkg_manager nmap
 $pkg_manager $go_lang
 
-
+#Go install
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/OWASP/Amass/v3/...@master
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
@@ -36,25 +37,28 @@ go install github.com/cgboal/sonarsearch/cmd/crobat@latest > /dev/null
 go install github.com/lc/gau/v2/cmd/gau@latest > /dev/null
 go install github.com/Emoe/kxss@latest > /dev/null
 go install github.com/tomnomnom/qsreplace@latest > /dev/null
-git clone https://github.com/ffuf/ffuf > /dev/null; cd ffuf; go install > /dev/null
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest > /dev/null
 go install github.com/hahwul/dalfox/v2@latest > /dev/null
 go install github.com/ferreiraklet/Jeeves@latest > /dev/null
 
-
+#Git Install
+mkdir gitclone; cd gitclone
 git clone https://github.com/tomnomnom/gf 
 git clone https://github.com/1ndianl33t/Gf-Patterns
+cd ..
 clear
 
+#Fun Part
 echo -e "[*]GF with good nature make life easy..." | pv -qL 8
 echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
 echo -e "[*]Proposing gf...."|pv -qL 8
 echo 'source /root/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
 echo -e "[*]Building Relation......"|pv -qL 8
-sudo mv ~/go/bin/* /bin/
-mkdir ~/.gf
 
-cp /gf/examples/* ~/.gf
-cp Gf-Patterns/* ~/.gf
+# Copying part
+sudo mv ~/go/bin/* /bin/ 2>/dev/null
+mkdir ~/.gf 2>/dev/null
+cp gitclone/gf/examples/* ~/.gf
+cp gitclone/Gf-Patterns/* ~/.gf
 echo -e "\n[*]Probe is ready to use...."|pv -qL 8
-cat .thanks.txt
+cat art/thanks.txt
