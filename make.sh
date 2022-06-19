ff543e9bc2c745aacaaf2e2b1bd306c9bbf1aada
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-06-20 00:02:51 +0530

    make.sh
    Signed-off-by: Anoint <72187543+whoami-anoint@users.noreply.github.com>

--- make.sh
+++ make.sh
@@ -29,6 +29,19 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 go install -v github.com/tomnomnom/gf@latest >/dev/null
+go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest > /dev/null
+go install github.com/OJ/gobuster/v3@latest > /dev/null
+go install github.com/tomnomnom/assetfinder@latest > /dev/null
+go install github.com/cgboal/sonarsearch/cmd/crobat@latest > /dev/null
+go install github.com/lc/gau/v2/cmd/gau@latest > /dev/null
+go install github.com/Emoe/kxss@latest > /dev/null
+go install github.com/tomnomnom/qsreplace@latest > /dev/null
+git clone https://github.com/ffuf/ffuf > /dev/null; cd ffuf; go install > /dev/null
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest > /dev/null
+go install github.com/hahwul/dalfox/v2@latest > /dev/null
+go install github.com/ferreiraklet/Jeeves@latest > /dev/null
+
+
 git clone https://github.com/tomnomnom/gf 
 git clone https://github.com/1ndianl33t/Gf-Patterns
 clear
