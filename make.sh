4a3e363072b2cc460cca22df284f6acdbeadd44d
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-06-21 23:17:10 +0545

    Update make.sh
    Signed-off-by: Anoint <72187543+whoami-anoint@users.noreply.github.com>

--- make.sh
+++ make.sh
@@ -5,14 +5,17 @@ os_type=`uname -r`
 if [[ $os_type == *"arch1"* ]];
 then
     pkg_manager="yay -S ";
-    # go_lang = "go"
+    go_lang="go"
 else 
     pkg_manager="sudo apt-get install";
     go_lang="golang"
 fi
 #Before running banner or other text pv is required
 
-$pkg_manager pv
+if [[ ! -f "/bin/pv" ]]
+then
+    $pkg_manager pv
+fi
 clear
 bash script/art.sh
 echo -e "\n\n[*]Installing Probe....." | pv -qL 8
@@ -29,36 +32,44 @@ go install -v github.com/OWASP/Amass/v3/...@master
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-go install -v github.com/tomnomnom/gf@latest >/dev/null
-go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest > /dev/null
-go install github.com/OJ/gobuster/v3@latest > /dev/null
-go install github.com/tomnomnom/assetfinder@latest > /dev/null
-go install github.com/cgboal/sonarsearch/cmd/crobat@latest > /dev/null
-go install github.com/lc/gau/v2/cmd/gau@latest > /dev/null
-go install github.com/Emoe/kxss@latest > /dev/null
-go install github.com/tomnomnom/qsreplace@latest > /dev/null
-go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest > /dev/null
-go install github.com/hahwul/dalfox/v2@latest > /dev/null
-go install github.com/ferreiraklet/Jeeves@latest > /dev/null
+go install -v github.com/tomnomnom/gf@latest 2>/dev/null
+go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest 2> /dev/null
+go install github.com/OJ/gobuster/v3@latest 2> /dev/null
+go install github.com/tomnomnom/assetfinder@latest 2> /dev/null
+go install github.com/cgboal/sonarsearch/cmd/crobat@latest 2> /dev/null
+go install github.com/lc/gau/v2/cmd/gau@latest 2> /dev/null
+go install github.com/Emoe/kxss@latest 2> /dev/null
+go install github.com/tomnomnom/qsreplace@latest 2> /dev/null
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest 2> /dev/null
+go install github.com/hahwul/dalfox/v2@latest 2> /dev/null
+go install github.com/ferreiraklet/Jeeves@latest 2> /dev/null
 
 #Git Install
-mkdir gitclone; cd gitclone
-git clone https://github.com/tomnomnom/gf 
-git clone https://github.com/1ndianl33t/Gf-Patterns
-cd ..
+mkdir gitclone 2>/dev/null; cd gitclone
+git clone https://github.com/tomnomnom/gf 2>/dev/null
+git clone https://github.com/1ndianl33t/Gf-Patterns 2>/dev/null
 clear
+#Setup Git tools
+#For GF
+mkdir ~/.gf 2>/dev/null
+cp gf/examples/* ~/.gf 2>/dev/null
+cp Gf-Patterns/* ~/.gf 2>/dev/null
+
+#Back to Home 
+cd ..
 
 #Fun Part
-echo -e "[*]GF with good nature make life easy..." | pv -qL 8
-echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
-echo -e "[*]Proposing gf...."|pv -qL 8
-echo 'source /root/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
-echo -e "[*]Building Relation......"|pv -qL 8
+if [[ ! -f "/bin/gf" ]]
+then
+    echo -e "[*]GF with good nature make life easy..." | pv -qL 8
+    echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+    echo -e "[*]Proposing gf...."|pv -qL 8
+    echo 'source /root/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+    echo -e "[*]Building Relation......"|pv -qL 8
+fi
 
-# Copying part
+# Copying part of go
 sudo mv ~/go/bin/* /bin/ 2>/dev/null
-mkdir ~/.gf 2>/dev/null
-cp gitclone/gf/examples/* ~/.gf
-cp gitclone/Gf-Patterns/* ~/.gf
+
 echo -e "\n[*]Probe is ready to use...."|pv -qL 8
 cat art/thanks.txt
