4b1bbdaf98575801b2d48ae0c2558a552f1fbfb9
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-06-21 23:05:25 +0530

    Add files via upload

---
+++ script/targets.py
@@ -0,0 +1,13 @@
+
+# Woking on input taking process 
+number = int(input("Enter number of Domains : "))
+target_list = []
+
+
+#For subfinder and amass
+with open("/tmp/targets.txt", 'w') as target:
+    for i in range(number):
+        target_list.append(input('['+str(i+1)+'] Enter Target : ')+'\n')
+        target.write(target_list[i])
+target.close()
+target_list = []
