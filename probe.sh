27777eb44f3269a8c3a1cb6ea4b730aaf59d6297
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-06-21 23:18:43 +0545

    Update probe.sh
    Signed-off-by: Anoint <72187543+whoami-anoint@users.noreply.github.com>

--- probe.sh
+++ probe.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 clear
-pv -qL 100 .banner.txt
-echo -e "\nEvery thing you need as a bug hunter" | pv -qL 25   
-
+# bash script/art.sh
 function error(){
     echo "*********************************************"
     echo "* * * You fool! Its like GIGO ;)  * * * * * *"
     echo "* * * * * * Invaild Input * * * * * * * * * *"
     echo "*********************************************"
 }
-#Creating Project 
+
+#Creating Project
 echo -n "Create project [y/N]"
 read response
 if [[ $response == "Y" || $response == "y" || $response == "yes" || $response == "Yes" ]]
@@ -27,25 +26,32 @@ else
     exit
 fi
 
+#Target 
+python3 script/targets.py
+
+#Probing
+#assestfinder
+
 function subdomain(){
-    subfinder -d  $target_link$i -t 200 -v -o sub1.txt
-    amass enum -src -ip -brute -d $target_link -o sub2.txt
+    #subfinder -dL  /tmp/targets.txt -t 200 -v -o /tmp/sub1.txt
+    #amass enum -src -ip -brute -df /tmp/targets.txt -o /tmp/sub2.txt
+    bash script/assestFinder.sh
     # sort sub1.txt sub2.txt | uniq -u > sub_uniq.txt
     # cat sub_uniq.txt |httpx -threads 200| tee -a subdomains.txt
     # cat subdomains.txt | waybackurls | tee -a waybackurls.txt
     # cat waybackurls.txt |gf xss| tee -a gfxss.txt
-    # notify -bulk -data waybackurls.txt -id subdomain 
 }
 
 if [[ $response == "Y" || $response == "y" || $response == "yes" || $response == "Yes" ]]
 then
     cd Projects/$pro_name
+    cd ../..
     subdomain
     
 
 elif [[ $response == "N" || $response == "n" || $response == "no" || $response == "No"  ]]
 then
-    cd Scan
+    cd Scan/Random
     subdomain
 
 else 
