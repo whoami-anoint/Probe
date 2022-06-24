84bacc013cfb428b9c8ff5a9844338067ef1e931
Selenophile (M7) <63031654+m7-Selenophile@users.noreply.github.com>
2022-06-24 23:21:22 +0545

    Update webarchive.py
    Signed-off-by: Selenophile (M7) <63031654+m7-Selenophile@users.noreply.github.com>

--- script/webarchive.py
+++ script/webarchive.py
@@ -8,30 +8,38 @@ bash('rm /tmp/sub4.txt /tmp/webarchive.txt 2>/tmp/null')
 get_body = ''
 def curl(target):
     global get_body
-    link = 'http://web.archive.org/cdx/search/cdx?url=*.'+target+'/*&output=text&fl=original&collapse=urlkey'
     b_obj = BytesIO()
     crl = pycurl.Curl()
-    crl.setopt(crl.URL, link)
+    crl.setopt(crl.URL, target)
     crl.setopt(crl.WRITEDATA, b_obj)
     crl.perform()
     crl.close()
     get_body = b_obj.getvalue()
-    data = get_body.decode('utf8')
-    with open('/tmp/webarchive.txt', 'a') as subdomain:
-        subdomain.write(data)
+    get_body = get_body.decode('utf8')
+
 
 #Target portion
 file = '/tmp/targets.txt'
+#opening target file
 with open(file, 'r') as domains:
     targets = domains.readlines()
+    
     for target in targets:
         target = target.strip()
         bash('clear')
         bash('echo [*]Finding archives for '+target+'.... | pv -qL 30')
-        curl(target)
+        link = 'http://web.archive.org/cdx/search/cdx?url=*.'+target+'/*&output=text&fl=original&collapse=urlkey'
+        curl(link) 
+
+        #Writing data in file 
+        with open('/tmp/webarchive.txt', 'a') as subdomain:
+            subdomain.write(get_body)
+        
         bash('echo [Completed] '+target+ '..... | pv -qL 30')
         bash('clear')
 domains.close
+
+#Filtering out the data obtained after curl
 cmd = 'cat /tmp/webarchive.txt | sed -e \'s_https*://__\' -e \"s/\/.*//\" | sort -u | tee -a /tmp/sub4.txt '
-cmd_sub = 'cat /tmp/sub4.txt'
 bash(cmd)
+
