9bc4057790b57e5fd86386e7ca04b0a66e6149e2
Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>
2022-07-23 14:30:26 +0545

    Update probe.sh
    Signed-off-by: Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>

--- probe.sh
+++ probe.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+
 clear
+
+#Some cool art stuff
+
 bash script/art.sh
 
-function error(){
-    echo "*********************************************"
-    echo "* * * You fool! Its like GIGO ;)  * * * * * *"
-    echo "* * * * * * Invalid Input * * * * * * * * * *"
-    echo "*********************************************"
-}
-
 #Creating Project
+
 echo -n "Create project [y/N]"
 read response
 if [[ $response == "Y" || $response == "y" || $response == "yes" || $response == "Yes" ]]
@@ -37,11 +35,13 @@ python3 script/targets.py
 
 function subdomain(){
     subfinder -dL  /tmp/targets.txt -t 200 -v -o /tmp/sub1.txt
+    echo -e "[*]This process may take some time " | pv -qL 30  
     amass enum -src -df /tmp/targets.txt -o /tmp/sub2.txt
     bash script/assestFinder.sh #It will create a file /tmp/sub3.txt
     anubis -f /tmp/targets.txt -o /tmp/sub3.txt
     python3 script/webarchive.py #It will create a file /tmp/sub4.txt
-    sort sub1.txt sub2.txt sub3.txt sub4.txt | uniq -u > sub_uniq.txt
+    cd Projects/$pro_name
+    sort /tmp/sub1.txt /tmp/sub2.txt /tmp/sub3.txt /tmp/sub4.txt | uniq -u > sub_uniq.txt
     cat sub_uniq.txt |httpx -threads 200| tee -a subdomains.txt
     cat subdomains.txt | waybackurls | tee -a waybackurls.txt
     cat waybackurls.txt |gf xss| tee -a gfxss.txt
@@ -49,8 +49,7 @@ function subdomain(){
 
 if [[ $response == "Y" || $response == "y" || $response == "yes" || $response == "Yes" ]]
 then
-    cd Projects/$pro_name
-    cd ../..
+
     subdomain
     
 
