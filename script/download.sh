c49d4d1d8c94a6b5746a34d1c393efc336d3f99d
Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>
2022-09-03 00:11:36 +0545

    Add files via upload

---
+++ script/download.sh
@@ -0,0 +1,6 @@
+mkdir ~/.probe 2>/dev/null
+mkdir ~/.probe/script 2>/dev/null
+wget https://raw.githubusercontent.com/whoami-anoint/Probe/main/script/gf.sh -O ~/.probe/script/gf.sh
+wget https://github.com/whoami-anoint/Probe/blob/main/script/configNotification.py -O ~/.probe/script/configNotification.py
+wget https://raw.githubusercontent.com/whoami-anoint/Probe/main/requirements.txt -O ~/.probe/requirements.txt
+pip3 install -r requirements.txt
