e493621374f2b5b30820e7021417b21d4acc23ec
Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>
2022-09-04 00:27:42 +0545

    Update download.sh
    Signed-off-by: Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>

--- script/download.sh
+++ script/download.sh
@@ -4,3 +4,4 @@ wget https://raw.githubusercontent.com/whoami-anoint/Probe/main/script/gf.sh -O 
 wget https://github.com/whoami-anoint/Probe/blob/main/script/configNotification.py -O ~/.probe/script/configNotification.py
 wget https://raw.githubusercontent.com/whoami-anoint/Probe/main/requirements.txt -O ~/.probe/requirements.txt
 pip3 install -r ~/.probe/requirements.txt
+clear
