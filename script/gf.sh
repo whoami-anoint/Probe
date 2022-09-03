df2943f94d9587ed8a6dcea90393b2480ef7c7e8
Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>
2022-09-04 00:27:15 +0545

    Update gf.sh
    Signed-off-by: Himansu Mahato <63031654+m7-Selenophile@users.noreply.github.com>

--- script/gf.sh
+++ script/gf.sh
@@ -3,7 +3,6 @@ mkdir gitclone 2>/dev/null; cd gitclone
 git clone https://github.com/tomnomnom/gf 2>/dev/null
 git clone https://github.com/1ndianl33t/Gf-Patterns 2>/dev/null
 
-clear
 #Setup Git tools
 #For GF
 mkdir ~/.gf 2>/dev/null
@@ -16,10 +15,7 @@ sudo cp ~/.local/bin/* /bin
 #Fun Part
 if [[ ! -f "/bin/gf" ]]
 then
-    echo -e "[*]GF with good nature make life easy..." | pv -qL 8
     echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
-    echo -e "[*]Proposing gf...."|pv -qL 8
     echo 'source /root/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
-    echo -e "[*]Building Relation......"|pv -qL 8
 fi
 sudo mv ~/go/bin/* /bin/ 2>/dev/null
